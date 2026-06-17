adfc6dec571f15dea102d61cd1131f312be1000b
saratou-rufai <saratou664@gmail.com>
2026-06-17 10:57:35 +0000

    sauvegarde du modele materiel

--- backend/materiels/admin.py
+++ backend/materiels/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Categorie, Materiel
+
+# On enregistre les modèles pour qu'ils soient visibles dans le back-office
+admin.site.register(Categorie)
+admin.site.register(Materiel)
